17b45602ab48039080e7e422eed5e3ac42bc654e
Irangaumayantha2001 <iamiranga9@gmail.com>
2024-02-29 19:15:24 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { SQLiteProvider } from "expo-sqlite/next";
+import { ActivityIndicator, Text, View } from "react-native";
+import * as FileSystem from "expo-file-system";
+import { Asset } from "expo-asset";
+import fonts from "./config/fonts";
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import Navigation from "./navigation";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+
+const loadDatabase = async () => {
+  const dbName = "mySQLiteDB.db";
+  const dbAsset = require("./assets/mySQLiteDB.db");
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+  if (!fileInfo.exists) {
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      { intermediates: true }
+    );
+    await FileSystem.downloadAsync(dbUri, dbFilePath);
+  }
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [dbLoaded, setDbLoaded] = React.useState<boolean>(false);
+  const [fontsLoaded] = useFonts(fonts);
+
+  React.useEffect(() => {
+    loadDatabase()
+      .then(() => setDbLoaded(true))
+      .catch((e) => console.error(e));
+  }, []);
+
+  if (!dbLoaded)
+    return (
+      <View style={{ flex: 1 }}>
+        <ActivityIndicator size={"large"} />
+        <Text>Loading Database...</Text>
+      </View>
+    );
+  return !fontsLoaded ? null : (
+    <SafeAreaProvider>
+      <React.Suspense
+        fallback={
+          <View style={{ flex: 1 }}>
+            <ActivityIndicator size={"large"} />
+            <Text>Loading Database...</Text>
+          </View>
+        }
+      >
+        <SQLiteProvider databaseName="mySQLiteDB.db" useSuspense>
+          <Navigation />
+          <StatusBar />
+        </SQLiteProvider>
+      </React.Suspense>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
